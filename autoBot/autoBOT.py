d725a10cfdcc2da4b872a1c37efab4fed66fdee7
Donovan <donnyscript@gmail.com>
2022-01-25 16:42:07 -0500

    ScrapeTest

--- autoBot/autoBOT.py
+++ autoBot/autoBOT.py
@@ -1,6 +1,8 @@
 import webbrowser
+
 webbrowser.open('https://www.iaai.com/Vehiclelisting/Ford?url=O3srmcEMyrv8m2S%2fZaOB64QzOTaw9zyS4R2TkHvkVeE%3d')
 
 webbrowser.open('https://www.copart.com/lotSearchResults?free=true&query=&searchCriteria=%7B%22query%22:%5B%22*%22%5D,%22filter%22:%7B%22FUEL%22:%5B%22fuel_type_desc:%5C%22DIESEL%5C%22%22%5D,%22MAKE%22:%5B%22lot_make_desc:%5C%22FORD%5C%22%22%5D,%22MODL%22:%5B%22lot_model_desc:%5C%22F-250%5C%22%22,%22lot_model_desc:%5C%22F%20350%5C%22%22,%22lot_model_desc:%5C%22F-350%5C%22%22,%22lot_model_desc:%5C%22F250%5C%22%22,%22lot_model_desc:%5C%22F250%20SUPER%5C%22%22,%22lot_model_desc:%5C%22F350%5C%22%22,%22lot_model_desc:%5C%22F250%20XLT%5C%22%22,%22lot_model_desc:%5C%22F350%20SUPER%5C%22%22,%22lot_model_desc:%5C%22F450%5C%22%22%5D%7D,%22searchName%22:%22%22,%22watchListOnly%22:false,%22freeFormSearch%22:false%7D')
 
 webbrowser.open('https://www.facebook.com/marketplace/108091592558812/search?query=diesel%20truck')
+
